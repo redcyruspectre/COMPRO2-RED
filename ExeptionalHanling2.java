0bec29ab560efc32515142e649424aed820321a1
redcyruspectre <redcyrus.oloresisimo@lorma.edu>
2026-01-27 13:55:17 +0800

    week 3 update

---
+++ ExeptionalHanling2.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class ExeptionalHanling2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+
+        int number;
+
+        try {
+            number = sc.nextInt();
+            int sum = number + 5;
+
+        } catch (Exception e) {
+            System.out.println("Invalid Number");
+        }
+
+        System.out.println("Try another number");
+        number = inputNumber();
+
+        System.out.println("Thank for using the program. Good bye.");
+        sc.close();
+
+    }
+
+    public static int inputNumber() {
+        int number = 0;
+
+        Scanner input = new Scanner(System.in);
+        for (;;) {  //infint loop
+            try {
+                number = inputNumber();
+                return number;
+                
+            } catch (Exception e) {
+                input.nextLine();
+                System.out.println("Invalid number");
+
+            }
+        }
+
+    }
+}
---
+++ ExeptionalPractice1.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+public class ExeptionalPractice1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double[][] matrix = new double[3][4];
+
+        System.out.println("Enter a 3-by-4 matrix row by row: ");
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                matrix[i][j] = input.nextDouble();
+            }
+        }
+
+        for (int col = 0; col < 4; col++) {
+            System.out.println(
+                    "Sum of the elements at column " + col + " is " + sumColumn(matrix, col));
+        }
+
+        input.close();
+    }
+
+    public static double sumColumn(double[][] m, int columnIndex) {
+        double sum = 0;
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i][columnIndex];
+        }
+        return sum;
+    }
+
+}
