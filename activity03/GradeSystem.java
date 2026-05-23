e50f0cdc48fe7500c56e7e59144ec746115eaf2a
redcyruspectre <redcyrus.oloresisimo@lorma.edu>
2026-05-24 07:33:45 +0800

    activity 3, Nested Menu

--- activity03/GradeSystem.java
+++ activity03/GradeSystem.java
@@ -1,7 +1,6 @@
-package activity3;
 import java.util.Scanner;
 import java.util.ArrayList;
- 
+  
 public class GradeSystem {
     // Storage for grades of each subject
     private static ArrayList<Double> subject1Grades = new ArrayList<>();
